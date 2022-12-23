ab32318723de57cd28fcfc713a8f2ba7a296e1cc
chrisyy <1017975501@qq.com>
2022-12-23 13:18:17 +0800

    修复过长报错的问题

--- nonebot_plugin_gpt3/__init__.py
+++ nonebot_plugin_gpt3/__init__.py
@@ -55,6 +55,16 @@ class Session:
         else:
             prompt = self.preset + restart_sequence + msg + start_sequence
 
+        token_len = len(tokenizer.encode(prompt))
+        logger.debug(token_len)
+
+        while token_len > 4096 - gpt3_max_tokens:
+            logger.debug("长度超过4096 - max_token，删除最早的一次会话")
+            del self.conversation[0]
+            prompt = self.preset + ''.join(self.conversation) + msg
+            token_len = len(tokenizer.encode(prompt))
+
+
         global api_index
         # 一个api失效时尝试下一个
         for i in range(len(api_key_list)):
@@ -69,19 +79,14 @@ class Session:
                 logger.error(f"API {api_index + 1}: 出现错误")
 
         if ok:
-            self.conversation.append(f"{msg} {start_sequence}{res}{restart_sequence}")
+            self.conversation.append(f"{msg}{start_sequence}{res}{restart_sequence}")
         else:
             # 超出长度或者错误自动重置
             self.reset()
 
-        token_len = len(tokenizer.encode(prompt))
-        logger.debug(f"token : {token_len}")
-        if token_len > 3000:
-            len_msg = f"\n字数: {token_len}"
-        else:
-            len_msg = ''
 
-        return res + len_msg
+
+        return res
 
     # 连续会话
     async def continuous_session(self):
@@ -105,7 +110,8 @@ from nonebot.log import logger
 from nonebot_plugin_htmlrender import md_to_pic
 
 
-@on_command("重置会话", aliases={"刷新", "重置"},priority=10, block=True, **need_at).handle()
+reset_c = on_command("重置会话", aliases={"刷新", "重置"},priority=10, block=True, **need_at)
+@reset_c.handle()
 async def _(matcher: Matcher, event: MessageEvent, arg: Message = CommandArg()):
     print(Message)
     session_id = event.get_session_id()
@@ -115,7 +121,8 @@ async def _(matcher: Matcher, event: MessageEvent, arg: Message = CommandArg()):
 
 
 
-@on_command("重置人格", priority=10, block=True, **need_at).handle()
+reset = on_command("重置人格", priority=10, block=True, **need_at)
+@reset.handle()
 async def _(matcher: Matcher, event: MessageEvent, arg: Message = CommandArg()):
     session_id = event.get_session_id()
     msg = arg.extract_plain_text().strip()
@@ -123,15 +130,20 @@ async def _(matcher: Matcher, event: MessageEvent, arg: Message = CommandArg()):
     await matcher.finish("人格已重置")
 
 
-@on_command("设置人格", aliases={"人格设置"}, priority=10, block=True, **need_at).handle()
+set_preset = on_command("设置人格", aliases={"人格设置"}, priority=10, block=True, **need_at)
+@set_preset.handle()
 async def _(matcher: Matcher, event: MessageEvent, arg: Message = CommandArg()):
     session_id = event.get_session_id()
     msg = arg.extract_plain_text().strip()
+    if not msg:
+        await matcher.finish("人格不能为空")
+
     get_user_session(session_id).set_preset(msg)
     await matcher.finish("设置成功")
 
 
-@on_command("导出会话", aliases={"导出对话"}, priority=10, block=True, **need_at).handle()
+dump = on_command("导出会话", aliases={"导出对话"}, priority=10, block=True, **need_at)
+@dump.handle()
 async def _(matcher: Matcher, event: MessageEvent):
     session_id = event.get_session_id()
     await matcher.finish(get_user_session(session_id).dump_user_session(), at_sender=True)
@@ -141,7 +153,8 @@ user_lock = {}
 
 
 # 基本聊天
-@on_command(priority=100, block=True, **matcher_params).handle()
+gpt3 = on_command(priority=100, block=True, **matcher_params)
+@gpt3.handle()
 async def _(matcher: Matcher, event: MessageEvent, arg: Message = CommandArg()):
     session_id = event.get_session_id()
     msg = arg.extract_plain_text().strip()
@@ -201,6 +214,7 @@ async def handle_city(event: MessageEvent, prompt: Message = Arg(), msg : str = 
         get_user_session(session_id).reset_preset()
         await chat_gpt3.reject_arg('prompt', '人格已重置')
     elif msg in reset:
+        get_user_session(session_id).reset()
         await chat_gpt3.reject_arg('prompt', '会话已重置')
 
     if msg not in end_conversation:
